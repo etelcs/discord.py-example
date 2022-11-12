c64c9058b970717f618e39f6359a3027a9059efc
etel <107803658+etelcn@users.noreply.github.com>
2022-11-12 00:55:34 +0000

    Update template.py

--- src/template.py
+++ src/template.py
@@ -32,6 +32,7 @@ async def hey(ctx):
     #gets channel name from ctx (where !hey was executed) and sends a message
     await ctx.send("Hello!")
 
+#slash-command
 @bot.tree.command(name="latency", description="shows the bot latency")
 async def latency(interaction: discord.Interaction):
   #ephemeral=True - shows the message to the command sender only
